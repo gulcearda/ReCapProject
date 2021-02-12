d64055c284b3cffda9f7127607e4967dbfd2a139
gulcearda <77541794+gulcearda@users.noreply.github.com>
2021-02-12 23:44:43 +0300

    8.Ödev bitti

---
+++ Business/Concrete/ColorManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        public void Add(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Color> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Color color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/SqlServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class SqlServer : DbContext
+    {
+        //projenin hangi veri tabanıyla ilişkili olduğunu gösterir.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=175.45.2.12; Database=SqlServer; Trusted_Connection=true");
+            //Sql serverın bulunduğu ip adresi yazılı bende hala yok Macbook:(
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+
+    }
+}
