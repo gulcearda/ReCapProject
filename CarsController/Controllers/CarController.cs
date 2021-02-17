501184c9b98d80c6557d11fb6ecb6873e6718788
gulcearda <77541794+gulcearda@users.noreply.github.com>
2021-02-17 20:51:55 +0300

    11. Gun odev

---
+++ CarsController/Controllers/CarController.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        //Loose coupled
+        //gevşek bağlılık bir bağlılık var ama soyuta, manager değişse mesela problem olmaz.
+        ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        //bunu yazmamıza rağmen gelmedi unable resolve dedi çünkü veriye constructerdan erişilmez.
+        //yukarı da somut referans yok bu nedenle bunu çözmek için IoC container kullanılır.
+
+
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Swagger bize dokümantasyon verilir bu apilerin nasıl ne amaçla kullanıldığına dair
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+
+            //Dependency chain --
+            //ICarService carService = new CarManager(new EfCarDal());
+            //var result = carService.GetAll();
+            //return result.Data;
+
+            //hoca bunu yazdı ve bende çizdi ama hocada çizmedi. Burada tablolar gelecek.
+            //gelmediğinde hoca icarservise breakpoint koydu.ama bunu da bozdu.
+            //çünkü sistemde bir bakım koymuştu hoca bu engelliyordu.
+            //hoca buna komment koyup kaldırdı.
+
+
+            //[HttpGet]
+            //public string Get()
+            //{
+            //    return "Merhaba";
+            //};
+
+            //Hocanın diğer yazıkları; bende çalışmıyor
+            //return new List<Car>
+            //{
+            //  new Car{CarId=1, CarName="Hyundai"},
+            //}
+            //bunların hepsi httpgetin içinde
+        }
+
+
+        [HttpPost("add")]
+
+        public IActionResult Add(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+            //karşı taraftan veri alırız ve sisteme ekleriz.
+        }
+
+        [HttpGet("GetById")]
+
+        public IActionResult GetById(int id)
+        {
+            var result = _carService.GetCarsByBrandId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+
+            //ife breakpoint koyuyoruz.
+            //hata alırız. Çünkü id istediğimiz için bunu anlamıyor
+            //id istemeliyiz en güzeli isimlendirmedir.
+            //isimleri değiştirdik ve şimdi çağırıyoruz. Postmanda sona isimlerini yazarız kodlamada 
+        }
+
+    }
+}
---
+++ CarsController/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CarsController
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //Autofac, Ninject, CastleWindsor, StructureMapi LightInject, DryInject -->IoC Conteiner
+            //AoP yapılacak
+            services.AddControllers();
+            services.AddSingleton<ICarService, CarManager>();
+            //arka planda bizim için newliyor ve paketleyip gönderiyor bize.
+            services.AddSingleton<ICarDal, EfCarDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
